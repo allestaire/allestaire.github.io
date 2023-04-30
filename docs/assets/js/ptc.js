4c6d7e02ced3393b2810d6e3dd36fe45261b5148
Al Lestaire Acasio <allestaire.acasio@fligno.com>
2023-04-30 08:40:34 +0800

    :bug: ptc to orentation

--- docs/assets/js/ptc.js
+++ docs/assets/js/ptc.js
@@ -92,12 +92,12 @@ const settings = {
 };
 
 const facePositions = {
-  f: {x: 1, y: 1},
-  b: {x: 3, y: 1},
-  r: {x: 2, y: 1},
-  l: {x: 0, y: 1},
-  u: {x: 1, y: 0},
-  d: {x: 1, y: 2}
+  pz: {x: 1, y: 1},
+  nz: {x: 3, y: 1},
+  px: {x: 2, y: 1},
+  nx: {x: 0, y: 1},
+  py: {x: 1, y: 0},
+  ny: {x: 1, y: 2}
 };
 
 function loadImage() {
